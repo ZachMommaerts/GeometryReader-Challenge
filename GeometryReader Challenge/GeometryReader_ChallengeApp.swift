e365919de4958093752c36c051541484da7341e4
ZachMommaerts <zmommaerts@gmail.com>
2023-03-30 10:38:20 -0500

    Initial Commit

---
+++ GeometryReader Challenge/GeometryReader_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReader_ChallengeApp.swift
+//  GeometryReader Challenge
+//
+//  Created by Zach Mommaerts on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReader_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
