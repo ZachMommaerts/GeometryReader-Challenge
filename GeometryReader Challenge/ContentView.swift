2adb0a05c7f109a9e989eae41db7c267cf9b9bfe
ZachMommaerts <zmommaerts@gmail.com>
2023-03-30 12:05:56 -0500

    Create Rectangles and print info about width and size

--- GeometryReader Challenge/ContentView.swift
+++ GeometryReader Challenge/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            
+            GeometryReader{ geo in
+                
+                Rectangle()
+                    .foregroundColor(Color.green)
+                    .onTapGesture {
+                        print("width: \(geo.size.width), height: \(geo.size.height)")
+                    }
+                
+                Rectangle()
+                    .frame(width: geo.size.width*0.66, height: geo.size.height/4)
+                    .foregroundColor(Color.red)
+                    .position(x: geo.size.width/2, y: geo.size.height/2)
+                    .onTapGesture {
+                        print("width: \(geo.size.width*0.66), height: \(geo.size.height/4)")
+                    }
+            }
+            
+            GeometryReader{ geo in
+                
+                Rectangle()
+                    .frame(width: geo.size.width/2)
+                    .foregroundColor(Color.yellow)
+                    .onTapGesture {
+                        print("width: \(geo.size.width/2), height: \(geo.size.height)")
+                    }
+                
+                Rectangle()
+                    .frame(width: geo.size.width/2)
+                    .foregroundColor(Color.orange)
+                    .padding([.leading], geo.size.width/2)
+                    .onTapGesture {
+                        print("width: \(geo.size.width/2), height: \(geo.size.height)")
+                    }
+            }
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
